586c39890f4329acd3496924d08b01f950b0031b
chnadru123 <chandrashekhar.13ec012@pace.edu.in>
2023-04-19 07:38:06 +0000

    Planning complete changes

---
+++ planning/planning/doctype/sci_project_dpr/sci_project_dpr.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Seyfert and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('SCI Project DPR', {
+	// refresh: function(frm) {
+
+	// }
+});
